dc5f80b3262e3cd8194a48ab6084ca65bb3b0365
Micah Martin <micahlmartin@gmail.com>
2023-10-20 12:14:22 -0400

    updated

--- helpers.sh
+++ helpers.sh
@@ -6,23 +6,29 @@ getSecret() {
   vault kv get --format=json $path | jq '.data.data'
 }
 
+vaultLogin() {
+  role_id="$1"
+  secret_id="$2"
+  vault write --format=json auth/approle/login role_id="${role_id}" secret_id="${secret_id}" | jq -r '.auth.client_token'
+}
+
 # Function to log debug messages
 log_debug() {
-    local message="$1"
-    if [ ! -z "$DEBUG" ]; then
-        echo "---------> DEBUG: $message"
-    fi
+  local message="$1"
+  if [ ! -z "$DEBUG" ]; then
+      echo "---------> DEBUG: $message"
+  fi
 }
 
 # Function to log a message
 log() {
-    local message="---------> INFO: $1"
-    echo "$message"
+  local message="---------> INFO: $1"
+  echo "$message"
 }
 
 # Function to log an error message and exit
 fatal() {
-    local message="$1"
-    >&2 echo "----> Error: $message"
-    exit 1
+  local message="$1"
+  >&2 echo "----> Error: $message"
+  exit 1
 }
